b3440e59824f0c4c5d3bcd9653c9ecf8d86a1c06
ojotuk <ojotuk14@yahoo.com>
2020-09-13 08:09:54 +0100

    fixed service worker code

--- asset/js/serviceWorker.js
+++ asset/js/serviceWorker.js
@@ -1,15 +1,17 @@
 // we register service worker here (sw.js)
 
 // check if browser support service worker
-if('serviceWorker' in navigator){
-    //reg service worker step ONE
-    navigator.serviceWorker.register('/sw.js')
-        .then((reg)=>{
-           // console.log('service worker register',reg)
-        }).catch((err)=>{
-           // console.log('service worker not registered',err)
-        });
+if ("serviceWorker" in navigator) {
+  //reg service worker step ONE
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => {
+      console.log("service worker register", reg);
+    })
+    .catch((err) => {
+      // console.log('service worker not registered',err)
+    });
 
-    // step TWO
-    //install event for service worker on sw.js file
+  // step TWO
+  //install event for service worker on sw.js file
 }
